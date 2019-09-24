5b09a3ec5fd645d76041bd7a93361edb271ec574
Akash <41006458+akashdev99@users.noreply.github.com>
2019-09-24 11:43:10 +0530

    improvements

--- mtrainier.R
+++ mtrainier.R
@@ -1,9 +1,14 @@
 # Importing the dataset
 dataset = read.csv('Rainier_Weather.csv')
+# Importing the dataset
+
+dec = read.csv('December.csv')
+
 
 #splitting test and train
 training_set = dataset[1:364,]
 test_set = dataset[365:464,]
+
 
 #ordering dataset
 training_set<- training_set[seq(dim(training_set)[1],1),]
@@ -26,27 +31,41 @@ for(x in 1:100){
 }
 test_set = cbind(test_set,day=dates)
 
-#liner regressor
+
+#Polynomial regressor
 training_set$day2 =training_set$day^2
 training_set$day3 =training_set$day^3
 training_set$day4 =training_set$day^4
-
-
 regressor = lm(formula = Temperature.AVG ~ day+day2+day3+day4,
                data = training_set)
 
-#svr
 library(e1071)
-regressor = svm(formula = Temperature.AVG ~ day,
-                data = training_set,
+regressor2 = svm(formula =Solare.Radiation.AVG ~ day,
+                data =training_set,
                 type = 'eps-regression',
                 kernel = 'radial')
 
-library(randomForest)
-set.seed(1234)
-regressor = randomForest(x = training_set,
-                         y = training_set$Temperature.AVG,
-                         ntree = 500)
+# 
+# #svr
+# # library(e1071)
+# # regressor = svm(formula = Temperature.AVG ~ day,
+# #                 data = training_set,
+# #                 type = 'eps-regression',
+# #                 kernel = 'radial')
+
+# library(randomForest)
+# set.seed(1234)
+# regressor = randomForest(x = training_set,
+#                          y = training_set$Temperature.AVG,
+#                          ntree = 500)
+
+
+#month wise prediction
+
+
+
+
+
 
 # Visualising the Training set results
 library(ggplot2)
@@ -59,6 +78,19 @@ ggplot() +
   xlab('Date') +
   ylab('temperature')
 
+# Visualising the Training set results
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = training_set$day, y = training_set$Solare.Radiation.AVG),
+             colour = 'red') +
+  geom_line(aes(x = training_set$day, y = predict(regressor2, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('date vs temperature (Training set)') +
+  xlab('Date') +
+  ylab('Radiation')
+
+
+
 # Visualising the Test set results
 library(ggplot2)
 ggplot() +
@@ -69,4 +101,7 @@ ggplot() +
   ggtitle('date vs temperature (Training set)') +
   xlab('Date') +
   ylab('temperature')
+
+
+
 
