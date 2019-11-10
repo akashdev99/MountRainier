aa91779c885b74f9ff7a1b9ee865662c76986138
Akash <41006458+akashdev99@users.noreply.github.com>
2019-11-10 10:03:56 +0530

    [PREDICTION] done

--- mtrainier.R
+++ mtrainier.R
@@ -33,14 +33,34 @@ test_set = cbind(test_set,day=dates)
 training_set$day2 =training_set$day^2
 training_set$day3 =training_set$day^3
 training_set$day4 =training_set$day^4
-regressor = lm(formula = Temperature.AVG ~ day+day2+day3+day4,
+#temeprature poly
+regressor_temp = lm(formula = Temperature.AVG ~ day+day2+day3+day4,
                data = training_set)
+#solar SVM
+library(e1071)
+regressor_solar = svm(formula =Solare.Radiation.AVG ~ day,
+                data =training_set,
+                type = 'eps-regression',
+                kernel = 'radial')
 
-# library(e1071)
-# regressor2 = svm(formula =Solare.Radiation.AVG ~ day,
-#                 data =training_set,
-#                 type = 'eps-regression',
-#                 kernel = 'radial')
+#Ranom Forest humidity
+library(randomForest)
+set.seed(1234)
+training_set_humid =training_set[,-1:-2] 
+training_set_humid =training_set_humid[,-2:-4]
+training_set_humid =training_set_humid[,-3:-5]
+regressor_humid = randomForest(x = training_set_humid,
+                         y = training_set$Relative.Humidity.AVG,
+                         ntree = 500)
+#Ranom Forest windspeed
+library(randomForest)
+set.seed(1234)
+training_set_wind =training_set[,-1:-3] 
+training_set_wind =training_set_wind[,-2:-3]
+training_set_wind =training_set_wind[,-3:-5]
+regressor_wind = randomForest(x = training_set_wind,
+                              y = training_set$Wind.Speed.Daily.AVG,
+                              ntree = 500)
 
 # 
 # #svr
@@ -57,48 +77,155 @@ regressor = lm(formula = Temperature.AVG ~ day+day2+day3+day4,
 #                          ntree = 500)
 
 
-#month wise prediction
+
+
+# Visualising the Training set results solar radiation
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = training_set$day, y = training_set$ Solare.Radiation.AVG ),
+             colour = 'red') +
+  geom_line(aes(x = training_set$day, y = predict(regressor_solar, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('date vs Radiation (Training set)') +
+  xlab('Date') +
+  ylab('Radiation')
+
+# Visualising the Test set results solar radiation
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = test_set$day, y = test_set$Solare.Radiation.AVG),
+             colour = 'red') +
+  geom_line(aes(x = training_set$day, y = predict(regressor_solar, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('date vs Radiation (testing))') +
+  xlab('Date') +
+  ylab('radiation')
 
 
 
-
-
-
-# Visualising the Training set results
+# Visualising the Training set results temperature
 library(ggplot2)
 ggplot() +
   geom_point(aes(x = training_set$day, y = training_set$Temperature.AVG),
              colour = 'red') +
-  geom_line(aes(x = training_set$day, y = predict(regressor, newdata = training_set)),
+  geom_line(aes(x = training_set$day, y = predict(regressor_temp, newdata = training_set)),
             colour = 'blue') +
   ggtitle('date vs temperature (Training set)') +
   xlab('Date') +
   ylab('temperature')
 
-# Visualising the Training set results for solar radiation
-# library(ggplot2)
-# ggplot() +
-#   geom_point(aes(x = training_set$day, y = training_set$Solare.Radiation.AVG),
-#              colour = 'red') +
-#   geom_line(aes(x = training_set$day, y = predict(regressor, newdata = training_set)),
-#             colour = 'blue') +
-#   ggtitle('date vs Solar Radiation (Training set)') +
-#   xlab('Date') +
-#   ylab('Radiation')
 
-
-
-# Visualising the Test set results
+# Visualising the Test set results temperature
 library(ggplot2)
 ggplot() +
   geom_point(aes(x = test_set$day, y = test_set$Temperature.AVG),
              colour = 'red') +
-  geom_line(aes(x = training_set$day, y = predict(regressor, newdata = training_set)),
+  geom_line(aes(x = training_set$day, y = predict(regressor_temp, newdata = training_set)),
             colour = 'blue') +
   ggtitle('date vs temperature (Training set)') +
   xlab('Date') +
   ylab('temperature')
 
+# Visualising the Training set results humidity
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = training_set$day, y = training_set$ Relative.Humidity.AVG ),
+             colour = 'red') +
+  geom_line(aes(x = training_set$day, y = predict(regressor_humid, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('date vs Humidity (Training set)') +
+  xlab('Date') +
+  ylab('humidity')
 
+
+# Visualising the Test set results humidity
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = test_set$day, y = test_set$Relative.Humidity.AVG),
+             colour = 'red') +
+  geom_line(aes(x = training_set$day, y = predict(regressor_humid, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('date vs Humidity (testing))') +
+  xlab('Date') +
+  ylab('Humidity')
+
+# Visualising the Training set results windspeed
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = training_set$day, y = training_set$ Wind.Speed.Daily.AVG ),
+             colour = 'red') +
+  geom_line(aes(x = training_set$day, y = predict(regressor_wind, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('date vs Windspeed (Training set)') +
+  xlab('Date') +
+  ylab('Windspeed')
+
+
+# Visualising the Test set results windspeed
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = test_set$day, y = test_set$Wind.Speed.Daily.AVG),
+             colour = 'red') +
+  geom_line(aes(x = training_set$day, y = predict(regressor_wind, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('date vs Windspeed (testing))') +
+  xlab('Date') +
+  ylab('Windspeed')
+
+
+
+#INput from the user
+rang.start = readline(prompt="please enter the start=")
+rang.end = readline(prompt="please enter the end=")
+starts=strsplit(rang.start,split="/",fixed=TRUE)
+start.day =starts[[1]][1]
+start.month=starts[[1]][2]
+
+ends=strsplit(rang.end,split="/",fixed=TRUE)
+end.day = ends[[1]][1]
+end.month=ends[[1]][2]
+
+#predicting attributes
+pred_start = 1
+pred_end = 6
+dates_1 = vector()
+dates_2 =vector()
+dates_3 =vector()
+dates_4 =vector()
+x=1
+for(i in pred_start:pred_end){
+  dates_1[x]<-i
+  dates_2[x]<-i^2
+  dates_3[x]<-i^3
+  dates_4[x]<-i^4
+  x=x+1
+}
+day=dates_1
+day2=dates_2
+day3=dates_3
+day4=dates_4
+user_set_temp = data.frame(day,day2,day3,day4)
+user_set_solar = data.frame(day)
+range=pred_end - pred_start +1
+#randomforest data generation
+if(pred_start==1){
+  training_set_wind=training_set_wind[-(range+1):-365,]
+  training_set_humid=training_set_humid[-(range+1):-365,]
+} else if(pred_end ==365 ){
+  training_set_wind=training_set_wind[-1:-(pred_start-1),]
+  training_set_humid=training_set_humid[-1:-(pred_start-1),]
+} else {
+  training_set_wind=training_set_wind[-1:-(pred_start-1),]
+  training_set_wind=training_set_wind[-(range+1 ):-365,]
+  training_set_humid=training_set_humid[-1:-(pred_start-1),]
+  training_set_humid=training_set_humid[-(range +1 ):-365,]
+}
+
+
+#pedicting attributes
+pred_temp=predict(regressor_temp, data.frame(train_x =user_set_temp ))
+pred_solar=predict(regressor_solar, data.frame(train_x =user_set_solar ))
+pred_wind = predict(regressor_wind, newdata = training_set_wind)
+pred_humid = predict(regressor_humid, newdata = training_set_humid)
 
 
